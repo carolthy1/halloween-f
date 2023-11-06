59b020af8e7f2f7006c0d309f6375f55635b9f56
carolthy1 <142441042+carolthy1@users.noreply.github.com>
2023-11-06 17:39:46 -0300

    senhaerrada

--- login_cadastro/script.js
+++ login_cadastro/script.js
@@ -18,72 +18,71 @@ document.addEventListener("DOMContentLoaded", function () {
       cadastrarDiv.style.display = "none";
     }
   });
-});
 
-document.getElementById("send").addEventListener("click", function () {
-  var nome = document.getElementById("nome").value;
-  var password = document.getElementById("password").value;
+  document.getElementById("send").addEventListener("click", function () {
+    var nome = document.getElementById("nome").value;
+    var password = document.getElementById("password").value;
 
-  var data = {
-    nome: nome,
-    password: password,
-  };
+    var data = {
+      nome: nome,
+      password: password,
+    };
 
-  fetch("http://192.168.42.252:3000/api/login", {
-    // Alterado para o seu endereço IP e porta
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  })
-    .then((response) => {
-      if (response.ok) {
-        console.log("success");
-        window.location.href = "home.html";
-      } else {
-        // Login falhou, mostrar uma mensagem de erro
-        console.error("Credenciais inválidas");
-      }
+    fetch("http://172.16.31.24:3000/api/login", {
+      // Alterado para o seu endereço IP e porta
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
     })
-    .catch((error) => {
-      console.error("Erro:", error);
-    });
-});
+      .then((response) => {
+        if (response.ok) {
+          console.log("success");
+          window.location.href = "home.html";
+        } else {
+          // Login falhou, mostrar uma mensagem de erro
+          var errorMessageElement = document.getElementById("error-message");
+          errorMessageElement.textContent = "Credenciais inválidas";
+        }
+      })
+      .catch((error) => {
+        console.error("Erro:", error);
+      });
+  });
 
-document.getElementById("C_send").addEventListener("click", function () {
-  var nome = document.getElementById("C_nome").value;
-  var password = document.getElementById("C_password").value;
+  document.getElementById("C_send").addEventListener("click", function () {
+    var nome = document.getElementById("C_nome").value;
+    var password = document.getElementById("C_password").value;
 
-  var data = {
-    nome: nome,
-    password: password,
-  };
+    var data = {
+      nome: nome,
+      password: password,
+    };
 
-  fetch("http://192.168.42.252:3000/api/cadastrar", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data); // Mostra a resposta da API no console (opcional)
-
-      // Se a API confirmar o cadastro, redirecione para home.html
-      if (data.success) {
-        window.location.href = "home.html";
-      } else {
-        console.error("Erro ao cadastrar usuário");
-      }
+    fetch("http://172.16.31.24:3000/api/cadastrar", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
     })
-    .catch((error) => {
-      console.error("Erro:", error);
-    });
-});
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data); // Mostra a resposta da API no console (opcional)
 
-document.addEventListener("DOMContentLoaded", function () {
+        // Se a API confirmar o cadastro, redirecione para home.html
+        if (data.success) {
+          window.location.href = "home.html";
+        } else {
+          console.error("Erro ao cadastrar usuário");
+        }
+      })
+      .catch((error) => {
+        console.error("Erro:", error);
+      });
+  });
+
   // Obtém o botão de login com o ID 'send'
   const loginButton = document.getElementById("send");
 
