a897991c57d935884bae9c92a7a07d2d168e9230
Leofelixpro <118075509+Leofelixpro@users.noreply.github.com>
2023-11-21 15:19:53 -0300

    up

--- desafio1.js
+++ desafio1.js
@@ -78,3 +78,34 @@ document.addEventListener("DOMContentLoaded", function () {
 function redirectToHome() {
   window.location.href = "home.html";
 }
+
+document.addEventListener("keydown", function (event) {
+  // Verifica se a tecla pressionada é a tecla 'A' e o botão 'Ctrl' (ou 'Command' em MacOS)
+  if (
+    (event.key === "a" || event.key === "A") &&
+    (event.ctrlKey || event.metaKey)
+  ) {
+    event.preventDefault(); // Impede o comportamento padrão (seleção de texto)
+    console.log("Seleção de texto bloqueada.");
+  }
+
+  // Verifica se a tecla pressionada é a tecla F12
+  if (event.key === "F12") {
+    event.preventDefault(); // Impede o comportamento padrão (abertura das DevTools)
+    console.log("Tecla F12 bloqueada.");
+  }
+});
+
+document.addEventListener("contextmenu", function (event) {
+  event.preventDefault(); // Impede o menu de contexto (botão direito do mouse)
+  console.log("Menu de contexto bloqueado.");
+});
+window.addEventListener("resize", function () {
+  window.resizeTo(window.innerWidth, window.innerHeight);
+});
+function updateScore() {
+  scoreDisplay.textContent = `Pontuação: ${score}`;
+
+  // Salvar pontuação no localStorage
+  localStorage.setItem("score", score);
+}
--- home.js
+++ home.js
@@ -13,9 +13,27 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function irParaDesafio1() {
+  // Exibir messagebox com a mensagem do desafio
+  window.alert(
+    "Bem-vindo ao Desafio 1!\nEncontre 6 objetos relacionados ao Halloween."
+  );
+
+  // Redirecionar para a página do Desafio 1
   window.location.href = "desafio1.html";
 }
 
 function irParaDesafio2() {
   window.location.href = "desafio2.html";
 }
+function abrirCarrinho() {
+  window.location.href = "loja.html";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const scoreDisplay = document.getElementById("score-display");
+
+  const storedScore = localStorage.getItem("score");
+  if (storedScore) {
+    scoreDisplay.textContent = `Pontuação: ${storedScore}`;
+  }
+});
